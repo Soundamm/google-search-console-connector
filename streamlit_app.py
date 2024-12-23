3b10332c3ca0c95a343472d579316d14f8b230e9
Soundamm <165473741+Soundamm@users.noreply.github.com>
2024-12-23 21:03:31 +0000

    Vuelta a código original

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,8 +49,8 @@ with tab1:
     st.write("")
 
     # Convert secrets from the TOML file to strings
-    clientSecret ="191921890644-fm7o55ra56ciiimv4nt0a4k632btdkd3.apps.googleusercontent.com"
-    clientId ="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"
+    clientSecret ="191921890644-cs9g4o8prro05lc89m6t3k3drtu04kuf.apps.googleusercontent.com"
+    clientId ="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"
     redirectUri ="https://app-search-console-extractor.streamlit.app"
 
     st.markdown("")
