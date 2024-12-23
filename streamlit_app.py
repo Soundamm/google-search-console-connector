246f41031f887142c093c9d3d62211a24e9c7030
Soundamm <165473741+Soundamm@users.noreply.github.com>
2024-12-23 20:58:35 +0000

    Vuelta a código original

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,9 +49,9 @@ with tab1:
     st.write("")
 
     # Convert secrets from the TOML file to strings
-    clientSecret = str(st.secrets["installed"]["client_secret"])
-    clientId = str(st.secrets["installed"]["client_id"])
-    redirectUri = str(st.secrets["installed"]["redirect_uris"][0])
+    clientSecret ="191921890644-fm7o55ra56ciiimv4nt0a4k632btdkd3.apps.googleusercontent.com"
+    clientId ="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"
+    redirectUri ="https://app-search-console-extractor.streamlit.app"
 
     st.markdown("")
 
